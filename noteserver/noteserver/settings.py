9fa9b2f7634f749f90afa3a32c66785374e026a6
therus <rusanckzhenya@yandex.ru>
2025-12-20 16:14:26 +0300

    rendr

--- noteserver/noteserver/settings.py
+++ noteserver/noteserver/settings.py
@@ -96,12 +96,12 @@ WSGI_APPLICATION = 'noteserver.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+import dj_database_url
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+  "default": dj_database_url.config(conn_max_age=600)
 }
+
 
 
 # Password validation
